8d47680972e7b04f5a8c37b71a719f2ab00bd0b5
Bryan Lee <bryanmylee@gmail.com>
2024-01-12 00:12:29 +0800

    chore: standard format

--- source/main.cpp
+++ source/main.cpp
@@ -1,67 +1,64 @@
 #include <iostream>
 
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
-void ProcessInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+void ProcessInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
 
-void ErrorCallback(int error, const char *msg)
-{
-    std::string s;
-    s = " [" + std::to_string(error) + "] " + msg + '\n';
-    std::cerr << s << std::endl;
+void ErrorCallback(int error, const char *msg) {
+  std::string s;
+  s = " [" + std::to_string(error) + "] " + msg + '\n';
+  std::cerr << s << std::endl;
 }
 
-int main()
-{
-    glfwSetErrorCallback(ErrorCallback);
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-    glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+int main() {
+  glfwSetErrorCallback(ErrorCallback);
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+  glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    // Construct the window
-    GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL Template", nullptr, nullptr);
-    if (!window)
-    {
-        std::cout << "Failed to create the GLFW window\n";
-        glfwTerminate();
-    }
-
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD\n";
-        return -1;
-    }
-
-    // Handle view port dimensions
-    glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, [](GLFWwindow *window, int width, int height)
-                                   { glViewport(0, 0, width, height); });
-
-    // This is the render loop
-    while (!glfwWindowShouldClose(window))
-    {
-        ProcessInput(window);
-
-        glClearColor(1.00f, 0.49f, 0.04f, 1.00f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
+  // Construct the window
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "OpenGL Template", nullptr, nullptr);
+  if (!window) {
+    std::cout << "Failed to create the GLFW window\n";
     glfwTerminate();
-    return 0;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD\n";
+    return -1;
+  }
+
+  // Handle view port dimensions
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window,
+                                 [](GLFWwindow *window, int width, int height) {
+                                   glViewport(0, 0, width, height);
+                                 });
+
+  // This is the render loop
+  while (!glfwWindowShouldClose(window)) {
+    ProcessInput(window);
+
+    glClearColor(1.00f, 0.49f, 0.04f, 1.00f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
